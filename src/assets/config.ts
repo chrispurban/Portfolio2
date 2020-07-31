12312417ef2b7c7f5b2a90ede0b49218d07c77fd
Chris Urban <chrispurban@gmail.com>
2020-07-31 16:55:02 -0500

    some stylings for task list

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,2 +1,2 @@
-export const baseurl = 'http://localhost:8080/';
-//export const baseurl = 'https://worksplat.herokuapp.com/'
+//export const baseurl = 'http://localhost:8080/';
+export const baseurl = 'https://worksplat.herokuapp.com/'
