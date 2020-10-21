47c6c87de442afd4f1da5e380443811f2435a1d9
Chris Urban <chrispurban@gmail.com>
2020-10-20 19:42:00 -0500

    phrasing changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent implements OnInit {
       _id:"first",
       name:"Taktile",
       tech:["Angular", "MongoDB", "NodeJS", "Express", "Auth0", "Heroku"],
-      desc:"Task management in the style of a Kanban board, a pun on Lean's 'takt time' and the use of a grid interface.\n\nFormating of each task encourages users to describe specifically what weak points they've identified, rather than offering a predetermined solution; this helps to maintain scope/objectivity and open negotiating space for creative solutions when multiple teams get involved.\n\nLike recursion? Drop me an email to see how this app was used in its own development!"
+      desc:"Task management in the style of a Kanban board, a pun on Lean's 'takt time' and the use of a grid interface.\n\nFormating of each task encourages users to describe what weak points they've identified, rather than a predetermined solution; this helps to maintain scope/objectivity and open negotiating space when multiple teams get involved.\n\nLike recursion? Drop me an email to see how this app was used in its own development!"
     }
   ];
 
