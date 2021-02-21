db76d67d982a6faac4fef87986b2db4fdd1ef95b
Chris Urban <chrispurban@gmail.com>
2021-02-20 21:21:24 -0600

    removed commented message template, not doing that feature

--- server/models/task.js
+++ server/models/task.js
@@ -1,18 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-/** let messageSchema = new Schema({
-  content:{
-    type:String,
-    required:true
-  },
-  author:{
-    type:mongoose.Schema.Types.ObjectID,
-    ref:'User'
-  },
-  timestamps:true
-}); **/
-
 let taskSchema = new Schema(
   {
     subject:{
@@ -26,7 +14,6 @@ let taskSchema = new Schema(
     history:[{
       type:Object
     }],
-    //messages:[messageSchema],
     owner:{
       type:String,
       required: true
